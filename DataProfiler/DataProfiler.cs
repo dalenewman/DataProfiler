3515963806cc709b4e0b32d95f4d308fd20c644b
Dale Newman <dalenewman@gmail.com>
2014-05-17 02:22:55 -0400

    Initial commit.

---
+++ DataProfiler/DataProfiler.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JunkDrawer;
+using Transformalize.Configuration.Builders;
+using Transformalize.Libs.Rhino.Etl;
+using Transformalize.Main;
+
+namespace DataProfiler {
+    public class DataProfiler {
+
+        public Dictionary<string, Row> Profile(Result result) {
+
+            var profile = result.Fields.ToDictionary(field => field.Name, field => new Row() { { "type", field.Type } });
+
+            var aggregates = new Dictionary<string, bool> {
+                {"min", false},
+                {"max", false},
+                {"minlength", false},
+                {"maxlength", false},
+                {"count", true}
+            };
+
+            foreach (var pair in aggregates) {
+                AddToProfile(ref profile, result, pair.Key, pair.Value);
+            }
+
+            return profile;
+        }
+
+        private static Row GetBuilder(Result result, string aggregate, bool distinct) {
+            var builder = new ProcessBuilder(result.FileInformation.ProcessName)
+                .Star(result.FileInformation.ProcessName)
+                .Connection("input")
+                    .Provider("internal")
+                .Connection("output")
+                    .Provider("internal")
+                .Entity("profile")
+                    .DetectChanges(false)
+                    .InputOperation(new RowsOperation(result.Rows))
+                    .Group()
+                    .Field("group")
+                        .Input(false)
+                        .Default("group")
+                        .Aggregate("group")
+                        .PrimaryKey();
+
+            foreach (var field in result.Fields) {
+                builder
+                    .Field(field.Name)
+                    .Length(field.Length)
+                    .Type(field.Type)
+                    .Aggregate(aggregate)
+                    .Distinct(distinct);
+            }
+
+            return ProcessFactory.Create(builder.Process())[0].Run()["profile"].First();
+        }
+
+        private static void AddToProfile(ref Dictionary<string, Row> profile, Result result, string aggregate, bool distinct) {
+            var minRow = GetBuilder(result, aggregate, distinct);
+            foreach (var column in minRow.Columns.Where(c => !c.Equals("group"))) {
+                profile[column][aggregate] = minRow[column];
+            }
+        }
+    }
+}
---
+++ Test/Development.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using JunkDrawer;
+
+namespace Test {
+    [TestFixture]
+    public class Development {
+        [Test]
+        public void TestProfiler() {
+            File.WriteAllText(@"temp.txt", @"t1,t2,t3,t4
+Monday,10,1.1,1/1/2014
+Tuesday,11,2.2,2/1/2014
+Wednesday,12,3.3,3/1/2014
+Thursday,13,4.4,4/1/2014
+Friday,14,5.5,5/1/2014
+Saturday,15,6.6,6/1/2014");
+
+            var result = new FileImporter().Import(@"temp.txt");
+            Assert.AreEqual(',', result.FileInformation.Delimiter);
+            Assert.AreEqual(4, result.Fields.Count());
+            Assert.AreEqual(6, result.Rows.Count());
+
+            var profile = new DataProfiler.DataProfiler().Profile(result);
+
+            Assert.AreEqual(4, profile.Count());
+
+            Assert.AreEqual("string", profile["t1"]["type"]);
+            Assert.AreEqual("byte", profile["t2"]["type"]);
+            Assert.AreEqual("single", profile["t3"]["type"]);
+            Assert.AreEqual("datetime", profile["t4"]["type"]);
+
+            Assert.AreEqual("Friday", profile["t1"]["min"]);
+            Assert.AreEqual(10, profile["t2"]["min"]);
+            Assert.AreEqual(1.1f, profile["t3"]["min"]);
+            Assert.AreEqual(new DateTime(2014, 1, 1, 0, 0, 0, DateTimeKind.Local), profile["t4"]["min"]);
+
+            Assert.AreEqual("Wednesday", profile["t1"]["max"]);
+            Assert.AreEqual(15, profile["t2"]["max"]);
+            Assert.AreEqual(6.6f, profile["t3"]["max"]);
+            Assert.AreEqual(new DateTime(2014, 6, 1, 0, 0, 0, DateTimeKind.Local), profile["t4"]["max"]);
+
+            Assert.AreEqual(6, profile["t1"]["minlength"]);
+            Assert.AreEqual(2, profile["t2"]["minlength"]);
+            Assert.AreEqual(3, profile["t3"]["minlength"]);
+            Assert.AreEqual(20, profile["t4"]["minlength"]);
+
+            Assert.AreEqual(9, profile["t1"]["maxlength"]);
+            Assert.AreEqual(2, profile["t2"]["maxlength"]);
+            Assert.AreEqual(3, profile["t3"]["maxlength"]);
+            Assert.AreEqual(20, profile["t4"]["maxlength"]);
+
+        }
+
+    }
+}
